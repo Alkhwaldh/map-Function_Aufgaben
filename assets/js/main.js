8f1af5effdd043d3cd5ff9425021956a2fc69844
Alkhwaldh <khwaldh89@gamil.com>
2020-05-15 22:28:15 +0200

    3-Aufgaben_map()Function

---
+++ assets/js/main.js
@@ -0,0 +1,75 @@
+/* var fahrenheit = [0, 32, 45, 50, 75, 80, 99, 120];
+
+ var celisuis = fahrenheit.map(
+     function(F) 
+            {console.log((F-32) / 1.8000)}
+                            );
+
+
+
+ */
+
+/*  */
+/* var array = [
+    18,
+    16,
+    80,
+    51,
+    47,
+    38,
+    95,
+    42,
+    68,
+    61,
+    34,
+    51,
+    20,
+    17,
+    56,
+    31,
+    100,
+    6,
+    5,
+    30,
+    74,
+    97,
+    28,
+    99,
+    91,
+    27,
+    73,
+    12,
+    92,
+    6,
+    27,
+    71,
+    26,
+    15,
+    78
+    ];
+
+  let num =  array.map((num)=> (num * 2 )); //+ Math.floor(Math.random() *10)
+
+  let num2= num.map((num)=> ( num+Math.floor(Math.random() * 11)))
+
+  console.log(num);
+  console.log(num2)
+    
+
+
+ */
+
+
+ const food = ["Coca-Cola",
+ "Apfelsaft",
+ "Pepsi",
+ "Traubensaft",
+ "Sprite",
+ "Orangensaft",
+ "Red Bull Energy Drink",
+ "Fanta"
+ ]
+
+   x = food.map((item)=> item.toUpperCase())
+
+   console.log(x)
